43353c8af9a6dc0306d76c63ac650ec256075ac5
tanveerdar933 <tanveerdar933@gmail.com>
2022-12-19 00:00:01 +0500

    added max char functionality

--- myProgram.js
+++ myProgram.js
@@ -1,6 +1,6 @@
 function checkPalindrome() {
     const valueFromInput = document.getElementById("stringInput").value.trim();
-    if (valueFromInput) {
+    if (valueFromInput || valueFromInput.length <= 10) {
         const reversedString = valueFromInput.split("").reverse().join("");
         if (valueFromInput === reversedString) {
             document.getElementById("resultBox").innerHTML = "is palandrom";
@@ -8,6 +8,9 @@ function checkPalindrome() {
         else {
             document.getElementById("resultBox").innerHTML = "is not palandrom";
         }
+    }
+    else if (valueFromInput.length > 10) {
+        document.getElementById("resultBox").innerHTML = "String must be max 10 char";
     }
 }
 function clearScreen() {
